53e2ead50954123edd07cfc3c209bbb26dd46ed4
cgkennedy <curt.kennedy@kellogg.northwestern.edu>
2020-02-18 03:01:23 +0000

    Week 7

--- where2.rb
+++ where2.rb
@@ -4,6 +4,9 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
+   @lat = rand(-90.000..90.000)
+   @long = rand(-180.000..180.000)
   # lat: ± 90
   # long: ± 180
+  view "where2"
 end
