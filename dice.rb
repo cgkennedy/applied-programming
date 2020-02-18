fb72e53415f27f375052e8f457865c630d81a811
cgkennedy <curt.kennedy@kellogg.northwestern.edu>
2020-02-18 01:53:35 +0000

    Week 7

--- dice.rb
+++ dice.rb
@@ -5,4 +5,22 @@ before { puts "Parameters: #{params}" }
 
 get "/" do
   "<h1>Hello, world!</h1>"
+
+dice_A = rand(1..6)
+dice_B = rand(1..6)
+total = dice_A + dice_B
+puts "#{dice_A}"
+puts "#{dice_B}"
+puts "The total is #{total}"
+
+if total == 11 || total == 7
+    puts "You Win!"
+elsif total == 2 || total == 3 || total == 12
+    puts "YOU LOSE!"
+else 
+    puts "The point is #{total}"
 end
+
+
+end
+
